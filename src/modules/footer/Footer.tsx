22152fdf3d47cd6660b278b5e720e275ba35fb7b
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-27 20:13:48 +0200

    :sparkles: create footer module

---
+++ src/modules/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="bg-stone-900 text-stone-200 absolute bottom-0 left-0 w-full text-xs px-8 py-2">
+      <p>© {year} FAQMaker</p>
+    </footer>
+  );
+};
