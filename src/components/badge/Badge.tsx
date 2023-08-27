50ed9e2c5da13fff294ea66f3c519b45f6549613
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-27 20:13:16 +0200

    :sparkles: create badge component

---
+++ src/components/badge/Badge.tsx
@@ -0,0 +1,32 @@
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+import { forwardRef, HTMLAttributes } from "react";
+
+const badge = cva("badge", {
+  variants: {
+    variant: {
+      pill: ["bg-stone-900", "text-stone-200", "rounded-full"],
+    },
+    rounded: {
+      full: ["rounded-full"],
+    },
+    size: {
+      small: ["px-2"],
+    },
+  },
+});
+
+export interface BadgeProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badge> {}
+
+export const Badge = forwardRef<HTMLDivElement, BadgeProps>(
+  ({ className, variant, rounded, size, ...props }, ref) => (
+    <div
+      className={cn(badge({ variant, rounded, size }), className)}
+      ref={ref}
+      {...props}
+    />
+  )
+);
