be70d1a99605d5fd886d865172433883fcea249b
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-27 11:12:10 +0200

    :sparkles: create input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,13 @@
+import { forwardRef } from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return <input type={type} ref={ref} {...props} />;
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
