9975d94882fc9ffbe3991c1dd6b280e82a7b92bd
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-27 11:13:35 +0200

    :sparkles: add react-query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
