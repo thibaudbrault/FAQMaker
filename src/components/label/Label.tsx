1e8e0ebd068b0085793e912c6b225208e577180d
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-27 20:13:28 +0200

    :sparkles: create label component

---
+++ src/components/label/Label.tsx
@@ -0,0 +1,22 @@
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+import { forwardRef, ElementRef, ComponentPropsWithoutRef } from "react";
+
+const label = cva(
+  "leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+);
+
+export const Label = forwardRef<
+  ElementRef<typeof LabelPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof label>
+>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(label(), className)}
+    {...props}
+  />
+));
+Label.displayName = LabelPrimitive.Root.displayName;
