bfaf765fe6da00633cd37dcc7e1a060451f1ca0c
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-28 19:35:03 +0200

    :truck: move and rename cn function's file

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,7 +1,7 @@
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { ChevronDown } from "lucide-react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 
 const Accordion = AccordionPrimitive.Root;
--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -1,7 +1,7 @@
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils/cn";
 import { forwardRef, ElementRef, ComponentPropsWithoutRef } from "react";
 
 const label = cva(
