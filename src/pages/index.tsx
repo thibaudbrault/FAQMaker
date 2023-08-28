1c44459de9fb7ec6232d6daa17c44bbb0e72e760
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-28 19:41:52 +0200

    :recycle: get session details and move over modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,44 @@
-import { List, Search } from "@/modules";
+import { Footer, Header, List, Search } from "@/modules";
+import { prisma } from "lib/prisma";
+import { GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
 
-function Home() {
+function Home(props) {
   return (
     <>
-      <Search />
-      <List />
+      <Header email={props.email} />
+      <div className="my-12">
+        <Search />
+        <List tenantId={props.tenantId} />
+      </div>
+      <Footer />
     </>
   );
 }
 
 export default Home;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/`,
+        permanent: false,
+      },
+    };
+  }
+
+  const email =
+    typeof session.user?.email === `string` ? session.user?.email : undefined;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  return {
+    props: JSON.parse(JSON.stringify(user)),
+  };
+}
