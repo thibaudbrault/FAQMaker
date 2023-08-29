6823cdf87b16b78d240da38180c6279fdb0aa10e
Thibaud Brault <thibaud.brault@protonmail.com>
2023-08-29 20:20:44 +0200

    add tooltips + add logout button

--- src/modules/layout/header/Header.tsx
+++ src/modules/layout/header/Header.tsx
@@ -1,7 +1,8 @@
-import { Link } from "@/components";
-import { getUser } from "@/data";
-import { User } from "@prisma/client";
-import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { Link, Tooltip, TooltipContent, TooltipTrigger } from "@/components";
+import { useUser } from "@/hooks";
+import { cn } from "@/utils/cn";
+import { LogOut, Settings, User as UserIcon } from "lucide-react";
+import { signOut } from "next-auth/react";
 import { CreateQuestion } from "../../question";
 
 type Props = {
@@ -10,46 +11,72 @@ type Props = {
 };
 
 export const Header = ({ email, company }: Props) => {
-  const {
-    data: user,
-    isLoading,
-    isError,
-    error,
-  }: UseQueryResult<User, Error> = useQuery({
-    queryKey: ["user", email],
-    queryFn: () => getUser(email),
-  });
+  const { data: user, isLoading } = useUser(email);
 
-  if (isError && error instanceof Error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const tooltipClass = "bg-teal-700 text-stone-200 border border-stone-200";
 
   return (
-    <header className="flex justify-between items-center px-8 py-4 bg-stone-900 text-stone-200">
+    <header className="flex justify-between items-center px-8 py-4 bg-teal-700 text-stone-200">
       <h1>
         <Link href="/" className="text-4xl font-serif">
           {company}
         </Link>
       </h1>
-      <div className="flex gap-4 items-baseline">
-        {!isLoading && (
-          <ul className="flex gap-4">
-            <li>
-              <Link decoration="underline" href="/profile">
-                Profile
-              </Link>
-            </li>
-            {user.role === "Admin" && (
+      {user && (
+        <div className="flex gap-4 items-baseline">
+          {!isLoading && (
+            <ul className="flex gap-4">
               <li>
-                <Link decoration="underline" href="/settings">
-                  Settings
-                </Link>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Link
+                      decoration="none"
+                      href="/profile"
+                      className="flex items-center gap-1"
+                    >
+                      <UserIcon />
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent className={cn(tooltipClass)}>
+                    <p>Profile</p>
+                  </TooltipContent>
+                </Tooltip>
               </li>
-            )}
-          </ul>
-        )}
-        <CreateQuestion user={user} />
-      </div>
+              {user?.role === "Admin" && (
+                <li>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Link
+                        decoration="none"
+                        href="/settings"
+                        className="flex items-center gap-1"
+                      >
+                        <Settings />
+                      </Link>
+                    </TooltipTrigger>
+                    <TooltipContent className={cn(tooltipClass)}>
+                      <p>Settings</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </li>
+              )}
+              <li>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <button onClick={() => signOut()}>
+                      <LogOut />
+                    </button>
+                  </TooltipTrigger>
+                  <TooltipContent className={cn(tooltipClass)}>
+                    <p>Logout</p>
+                  </TooltipContent>
+                </Tooltip>
+              </li>
+            </ul>
+          )}
+          <CreateQuestion user={user} />
+        </div>
+      )}
     </header>
   );
 };
