c93cfd4d94acf0aaa14f05fac3545bc2d73d8077
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-05 20:02:29 +0200

    :fire: remove old layout component

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,7 +2,6 @@ export * from './list';
 export * from './question';
 export * from './search';
 export * from './answer';
-export * from './layout';
 export * from './settings';
 export * from './footer';
 export * from './header';
