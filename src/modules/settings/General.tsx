ef21ed58cb045d016f37cc1e35807eeb0b8c022e
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-05 20:04:13 +0200

    :heavy_minus_sign: remove treamor

--- src/modules/settings/General.tsx
+++ src/modules/settings/General.tsx
@@ -1,16 +1,5 @@
 import { Node } from '@prisma/client';
-import {
-  Card,
-  Col,
-  Divider,
-  Flex,
-  Grid,
-  Icon,
-  Metric,
-  TabPanel,
-  Text,
-  Title,
-} from '@tremor/react';
+import { Card, Col, Flex, Grid, Icon, Metric, Text } from '@tremor/react';
 import { HelpCircle } from 'lucide-react';
 
 type Props = {
@@ -19,44 +8,43 @@ type Props = {
 
 export const General = ({ nodes }: Props) => {
   return (
-    <TabPanel>
-      <Grid numItemsLg={6} className="gap-6 mt-6">
-        {/* Main section */}
-        <Col numColSpanLg={4}>
-          <Card className="h-full">
-            <div className="h-60" />
-          </Card>
-        </Col>
+    <p>placeholder</p>
+    // <Grid numItemsLg={6} className="gap-6 mt-6">
+    //   {/* Main section */}
+    //   <Col numColSpanLg={4}>
+    //     <Card className="h-full">
+    //       <div className="h-60" />
+    //     </Card>
+    //   </Col>
 
-        {/* KPI sidebar */}
-        <Col numColSpanLg={2}>
-          <div className="space-y-6">
-            <Card>
-              <Flex justifyContent="start" className="space-x-4">
-                <Icon
-                  icon={HelpCircle}
-                  variant="light"
-                  size="xl"
-                  color="transparent"
-                />
-                <div className="truncate">
-                  <Text>Question</Text>
-                  <Metric className="truncate">{nodes.length}</Metric>
-                </div>
-              </Flex>
-            </Card>
-            <Card>
-              <div className="h-24" />
-            </Card>
-            <Card>
-              <div className="h-24" />
-            </Card>
-            <Card>
-              <div className="h-24" />
-            </Card>
-          </div>
-        </Col>
-      </Grid>
-    </TabPanel>
+    //   {/* KPI sidebar */}
+    //   <Col numColSpanLg={2}>
+    //     <div className="space-y-6">
+    //       <Card>
+    //         <Flex justifyContent="start" className="space-x-4">
+    //           <Icon
+    //             icon={HelpCircle}
+    //             variant="light"
+    //             size="xl"
+    //             color="transparent"
+    //           />
+    //           <div className="truncate">
+    //             <Text>Question</Text>
+    //             <Metric className="truncate">{nodes.length}</Metric>
+    //           </div>
+    //         </Flex>
+    //       </Card>
+    //       <Card>
+    //         <div className="h-24" />
+    //       </Card>
+    //       <Card>
+    //         <div className="h-24" />
+    //       </Card>
+    //       <Card>
+    //         <div className="h-24" />
+    //       </Card>
+    //     </div>
+    //   </Col>
+    // </Grid>
   );
 };
