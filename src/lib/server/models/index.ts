fef8d938f87bcb7e2f072c510f1fdef59a6f03f8
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-17 18:07:46 +0200

    :art: add export of new functions

--- src/lib/server/models/index.ts
+++ src/lib/server/models/index.ts
@@ -4,3 +4,4 @@ export * from './nodes';
 export * from './questions';
 export * from './answers';
 export * from './tenant';
+export * from './tags';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,5 @@ export * from './routing';
 export * from './keys';
 export * from './prisma';
 export * from './dateFormat';
+export * from './arraysEqual';
+export * from './generatePassword';
