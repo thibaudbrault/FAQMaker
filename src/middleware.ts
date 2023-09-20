04866f912d2c75c4998d9ebbcfb7293a613c75a4
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-20 10:15:28 +0200

    :recycle: add exception for register page in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,14 @@ import { withAuth } from 'next-auth/middleware';
 import { Routes } from './utils';
 
 export default withAuth({
+  callbacks: {
+    authorized: async ({ req, token }) => {
+      const pathname = req.url;
+      if (token) return true;
+      if (pathname === `${process.env.NEXT_PUBLIC_SITE_URL}/register`)
+        return true;
+    },
+  },
   pages: {
     signIn: Routes.SITE.LOGIN,
   },
