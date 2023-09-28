adbc2383eccf69b391050bd7cbc572eecff7ce54
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-28 12:10:07 +0200

    :closed_lock_with_key: add environment variables for stripe and google auth

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -5,6 +5,11 @@ declare global {
 
       NEXTAUTH_URL: string;
       NEXTAUTH_SECRET: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+
+      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
+      STRIPE_SECRET_KEY: string;
     }
   }
 }
