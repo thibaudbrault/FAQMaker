a979ec3eadbf6b7cfa64c05fd026b7061b9cb273
Thibaud Brault <thibaud.brault@protonmail.com>
2023-09-28 12:06:22 +0200

    :wrench: add images domains in next config file

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = removeImports({
     // trailingSlash: true,
     transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
     swcMinify: true,
+    images: {
+        domains: ['lh3.googleusercontent.com']
+    },
     webpack: (config) => {
         config.experiments = { ...config.experiments, topLevelAwait: true };
         return config;
