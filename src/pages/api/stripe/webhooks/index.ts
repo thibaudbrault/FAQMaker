a9c9c8b9d9763b25ae41ae3c553fe9b95fecfde3
Thibaud Brault <thibaud.brault@protonmail.com>
2023-10-14 17:57:35 +0200

    :truck: move webhooks file to own folder

--- src/pages/api/stripe/webhooks/index.ts
+++ src/pages/api/stripe/webhooks/index.ts
@@ -38,7 +38,10 @@ export default async function handler(
     'checkout.session.completed',
     'payment_intent.succeeded',
     'payment_intent.payment_failed',
+    'customer.subscription.updated',
   ];
+
+  console.log('first');
 
   if (permittedEvents.includes(event.type)) {
     let data;
@@ -55,6 +58,10 @@ export default async function handler(
               subscriptionId,
             },
           });
+          break;
+        case 'customer.subscription.updated':
+          const customer = event.data.object as Stripe.Subscription;
+          console.log('🚀 ~ file: index.ts:62 ~ customer:', customer);
           break;
         case 'payment_intent.payment_failed':
           data = event.data.object as Stripe.PaymentIntent;
