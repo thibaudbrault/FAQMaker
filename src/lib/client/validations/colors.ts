552046d7c67bab1dbb37524077459e229a3412a6
Thibaud Brault <thibaud.brault@protonmail.com>
2023-12-26 16:07:26 +0100

    :safety_vest: update zod color validation with regex check

--- src/lib/client/validations/colors.ts
+++ src/lib/client/validations/colors.ts
@@ -1,6 +1,18 @@
 import { z } from 'zod';
 
 export const colorsClientSchema = z.object({
-  foreground: z.string().trim().url({ message: 'Invalid URL' }),
-  background: z.string().trim().url({ message: 'Invalid URL' }),
+  foreground: z
+    .string()
+    .trim()
+    .regex(
+      new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/),
+      'Invalid hex color code',
+    ),
+  background: z
+    .string()
+    .trim()
+    .regex(
+      new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/),
+      'Invalid hex color code',
+    ),
 });
