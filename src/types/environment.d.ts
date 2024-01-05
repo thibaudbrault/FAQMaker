40751afd9f763bbd40f1b0d1252147745cb510d1
Thibaud BRAULT <thibaud.brault@gide.net>
2024-01-05 16:28:15 +0100

    :label: add new variables used in Playwright

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -11,6 +11,9 @@ declare global {
       NEXT_PUBLIC_SITE_URL: string;
       SEED_USER_EMAIL: string;
 
+      TEST_EMAIL: string;
+      TEST_PASSWORD: string;
+
       NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string;
       STRIPE_SECRET_KEY: string;
       STRIPE_WEBHOOK_SECRET: string;
