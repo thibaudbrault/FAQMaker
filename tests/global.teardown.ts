d96bc179fe178d2646d27eea8fd5d96a9750bdc9
Thibaud Brault <thibaud.brault@protonmail.com>
2024-01-06 18:34:34 +0100

    :white_check_mark: create teardown steps

---
+++ tests/global.teardown.ts
@@ -0,0 +1,55 @@
+import { test as teardown } from '@playwright/test';
+import prisma from '../lib/prisma';
+import { Tenant, User } from '@prisma/client';
+
+const getTenant = async () => {
+  const res = await prisma.tenant.findUnique({
+    where: { email: 'tenant@test.com' },
+  });
+  return res;
+};
+
+const getUser = async () => {
+  const res = await prisma.user.findUnique({
+    where: { email: process.env.TEST_EMAIL! },
+  });
+  return res;
+};
+
+const deleteTenant = async () => {
+  await prisma.tenant.delete({
+    where: { id: tenant?.id },
+  });
+};
+
+const deleteUser = async () => {
+  await prisma.user.delete({
+    where: { id: user?.id },
+  });
+};
+
+const deleteNode = async () => {
+  await prisma.answer.deleteMany({
+    where: { userId: user?.id },
+  });
+  await prisma.question.deleteMany({
+    where: { userId: user?.id },
+  });
+  await prisma.node.deleteMany({
+    where: { tenantId: tenant?.id },
+  });
+};
+
+let tenant: Tenant | null;
+let user: User | null;
+
+teardown.beforeEach(async () => {
+  tenant = await getTenant();
+  user = await getUser();
+});
+
+teardown('Empty db', async () => {
+  await deleteNode();
+  await deleteUser();
+  await deleteTenant();
+});
