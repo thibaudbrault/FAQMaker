40bd3aef96cd11bd3b9d66ed264ad8d7ba92269a
Thibaud Brault <thibaud.brault@protonmail.com>
2024-01-07 15:38:51 +0100

    :zap: split tests in two files

--- tests/nodes.test.ts
+++ tests/nodes.test.ts
@@ -7,26 +7,36 @@ test('Can ask a question', async ({ page }) => {
   await page.getByPlaceholder('New question').fill('This is a question');
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(
-    page.getByRole('heading', { name: 'This is a question' }),
+    page.getByRole('heading', { name: 'This is a question', exact: true }),
   ).toBeVisible();
 });
 
 test('Can answer a question', async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('heading', { name: 'This is a question' }).click();
+  await page
+    .getByRole('heading', { name: 'This is a question', exact: true })
+    .click();
   await page.getByRole('link', { name: 'Answer' }).click();
   await page.waitForURL(/\/question\/answer/);
   await page.getByRole('textbox').click();
-  await page.getByRole('textbox').fill('This an answer');
+  await page.getByRole('textbox').fill('This is an answer');
   await page.getByRole('button', { name: 'Submit' }).click();
-  await page.getByRole('heading', { name: 'This is a question' }).click();
-  await expect(page.getByText('This an answer')).toBeVisible();
+  await page
+    .getByRole('heading', { name: 'This is a question', exact: true })
+    .click();
+  await expect(page.getByText('This is an answer')).toBeVisible();
 });
 
 test('Can edit a question', async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('heading', { name: 'This is a question' }).click();
-  await page.getByRole('link', { name: 'Modify' }).click();
+  await page
+    .getByRole('heading', { name: 'This is a question', exact: true })
+    .click();
+  await page
+    .locator('li')
+    .filter({ hasText: 'This is a questionModify' })
+    .getByRole('link')
+    .click();
   await page.getByRole('button', { name: 'Edit' }).click();
   await page.getByRole('link', { name: 'Question' }).click();
   await page.waitForURL(/\/question\/edit/);
@@ -42,9 +52,13 @@ test('Can edit a question', async ({ page }) => {
 test('Can edit an answer', async ({ page }) => {
   await page.goto('/');
   await page
-    .getByRole('heading', { name: 'This is a modified question' })
+    .getByRole('heading', { name: 'This is a modified question', exact: true })
     .click();
-  await page.getByRole('link', { name: 'Modify' }).click();
+  await page
+    .locator('li')
+    .filter({ hasText: 'This is a modified question' })
+    .getByRole('link')
+    .click();
   await page.getByRole('button', { name: 'Edit' }).click();
   await page.getByRole('link', { name: 'Answer' }).click();
   await page.waitForURL(/\/question\/answer/);
