8fe19f40e79933953a15037de67be79760a2e65c
Thibaud BRAULT <thibaud.brault@gide.net>
2024-01-10 17:22:50 +0100

    :green_heart: create test for deleting user

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -18,10 +18,21 @@ test('Can modify a user', async ({ page }) => {
   await page.locator('header').getByRole('link').nth(2).click();
   await page.getByRole('tab', { name: 'Users' }).click();
   await page.getByRole('button', { name: 'Modify' }).nth(1).click();
+  await page.getByPlaceholder('Name', { exact: true }).fill('Test User');
   await page.getByPlaceholder('Email').fill('test.modified@gmail.com');
-  await page.pause();
   await page.getByLabel('Role').click();
   await page.getByLabel('User', { exact: true }).click();
+  await page.getByRole('button', { name: 'Update' }).click();
   await page.getByRole('button', { name: 'Close' }).click();
+  await expect(page.getByText('Test User')).toBeVisible();
   await expect(page.getByText('test.modified@gmail.com')).toBeVisible();
+});
+
+test('Can delete a user', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('header').getByRole('link').nth(2).click();
+  await page.getByRole('tab', { name: 'Users' }).click();
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await expect(page.getByText('Test User')).toBeHidden();
+  await expect(page.getByText('test.modified@gmail.com')).toBeHidden();
 });
