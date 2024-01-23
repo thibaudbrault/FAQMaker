ff9aca01bed4bce622245a3ea3d00bff2a90aa74
Thibaud BRAULT <thibaud.brault@gide.net>
2024-01-23 09:04:16 +0100

    :fire: remove useless tag

--- src/modules/settings/users/Users.tsx
+++ src/modules/settings/users/Users.tsx
@@ -41,53 +41,51 @@ export const Users = ({ userId, tenantId, plan }: Props) => {
   const iconStyle = 'w-9 h-9 m-3 inline-flex flex-shrink-0 items-center';
 
   return (
-    <section className="mx-auto w-11/12 md:w-3/4">
-      <ul className="flex list-none flex-col gap-4">
-        {users?.map((user) => (
-          <li
-            key={user.id}
-            className="rounded-md border border-stone-200 bg-default p-6 shadow-sm"
-          >
-            <div className="flex w-full flex-col justify-between gap-2 md:flex-row md:gap-0">
-              <div className="flex items-center justify-start">
-                {user.role === 'user' ? (
-                  <UserIcon className={iconStyle} />
-                ) : (
-                  <ShieldAlert className={iconStyle} />
-                )}
-                <div className="flex flex-col items-start">
-                  <h2 className="text-2xl">
-                    <b>{user.name}</b>
-                  </h2>
-                  <p>{user.email}</p>
-                </div>
-              </div>
-              <div className="flex items-center justify-center gap-2">
-                <UpdateUser user={user} tenantId={tenantId} />
-                {(user.role === 'user' || user.id !== userId) && (
-                  <Button
-                    variant="secondary"
-                    size="small"
-                    weight="semibold"
-                    className="lowercase"
-                    style={{ fontVariant: 'small-caps' }}
-                    onClick={() => handleDeleteUser(user.id)}
-                  >
-                    Delete
-                  </Button>
-                )}
+    <ul className="flex list-none flex-col gap-4">
+      {users?.map((user) => (
+        <li
+          key={user.id}
+          className="rounded-md border border-stone-200 bg-default p-6 shadow-sm"
+        >
+          <div className="flex w-full flex-col justify-between gap-2 md:flex-row md:gap-0">
+            <div className="flex items-center justify-start">
+              {user.role === 'user' ? (
+                <UserIcon className={iconStyle} />
+              ) : (
+                <ShieldAlert className={iconStyle} />
+              )}
+              <div className="flex flex-col items-start">
+                <h2 className="text-2xl">
+                  <b>{user.name}</b>
+                </h2>
+                <p>{user.email}</p>
               </div>
             </div>
-          </li>
-        ))}
-        <CreateUser tenantId={tenantId} />
-        <div className="flex items-center gap-4">
-          <div className="h-px flex-grow bg-negative" />
-          <p className="text-center text-xl font-bold uppercase">or</p>
-          <div className="h-px flex-grow bg-negative" />
-        </div>
-        <FileInput tenantId={tenantId} users={users} plan={plan} />
-      </ul>
-    </section>
+            <div className="flex items-center justify-center gap-2">
+              <UpdateUser user={user} tenantId={tenantId} />
+              {(user.role === 'user' || user.id !== userId) && (
+                <Button
+                  variant="secondary"
+                  size="small"
+                  weight="semibold"
+                  className="lowercase"
+                  style={{ fontVariant: 'small-caps' }}
+                  onClick={() => handleDeleteUser(user.id)}
+                >
+                  Delete
+                </Button>
+              )}
+            </div>
+          </div>
+        </li>
+      ))}
+      <CreateUser tenantId={tenantId} />
+      <div className="flex items-center gap-4">
+        <div className="h-px flex-grow bg-negative" />
+        <p className="text-center text-xl font-bold uppercase">or</p>
+        <div className="h-px flex-grow bg-negative" />
+      </div>
+      <FileInput tenantId={tenantId} users={users} plan={plan} />
+    </ul>
   );
 };
