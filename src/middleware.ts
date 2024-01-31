fdb8192a144a735c56d166b6f2580a7837cd05a1
Thibaud BRAULT <thibaud.brault@gide.net>
2024-01-31 09:45:26 +0100

    :recycle: refacto middleware to use native nextjs writing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,5 @@
-import { withAuth } from 'next-auth/middleware';
+export { default } from 'next-auth/middleware';
 
-import { Routes } from './utils';
-
-export default withAuth({
-  callbacks: {
-    authorized: async ({ req, token }) => {
-      const pathname = req.url;
-      if (token) return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/register`) return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/register/user`) return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/register/plan`) return true;
-      if (
-        pathname === `${process.env.NEXTAUTH_URL}/register/plan?status=cancel`
-      )
-        return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/register/confirm`)
-        return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/api/stripe/checkout`)
-        return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/api/stripe/webhooks`)
-        return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/api/stripe/customer`)
-        return true;
-      if (pathname === `${process.env.NEXTAUTH_URL}/api/tenant`) return true;
-    },
-  },
-  pages: {
-    signIn: Routes.SITE.LOGIN,
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+export const config = {
+  matcher: ['/', '/question', '/question/:path*', '/profile', '/settings'],
+};
