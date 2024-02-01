cdef7dc44929a61391c46ad4eeeb92c2155c243a
Thibaud Brault <77327446+thibaudbrault@users.noreply.github.com>
2024-02-01 23:04:11 +0100

    :iphone: responsive layout

--- src/layouts/LoginLayout.tsx
+++ src/layouts/LoginLayout.tsx
@@ -8,7 +8,7 @@ export const LoginLayout = ({ children }: Props) => {
   return (
     <main className="min-h-screen w-full bg-gradient-to-br from-black via-teal-950 to-teal-700">
       <section className="flex min-h-screen w-full items-center justify-center">
-        <div className="flex min-w-[500px] flex-col items-center gap-8 rounded-md bg-default p-8">
+        <div className="mx-auto flex w-11/12 flex-col items-center gap-8 rounded-md bg-default p-8 md:w-[500px]">
           {children}
         </div>
       </section>
