1c352fd85d6e44a0a7116b84803ed5cc7d8b82c3
Thibaud Brault <thibaud.brault@protonmail.com>
2024-02-04 17:23:09 +0100

    :bug: removed problematic function and use already existing equivalent

--- src/hooks/queries/nodes/useNodesCount.ts
+++ src/hooks/queries/nodes/useNodesCount.ts
@@ -1,18 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
+import { getNodesCount } from '@/lib';
 import { QueryKeys } from '@/utils';
-import prisma from 'lib/prisma';
-
-const getNodesCount = async (tenantId: string) => {
-  const nodes = await prisma.node.count({
-    where: { tenantId },
-  });
-
-  if (!nodes) return null;
-
-  return nodes;
-};
 
 export const useNodesCount = (tenantId: string) => {
   const query = useQuery<number, AxiosError>({
