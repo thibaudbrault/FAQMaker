996af8bcb7f59140a1ad36fbaea2e430cbda46d6
Thibaud BRAULT <thibaud.brault@gide.net>
2024-02-07 11:04:35 +0100

    :sparkles: add sentry

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require('next-remove-imports')();
-module.exports = removeImports({
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const nextConfig = removeImports({
     transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
     swcMinify: true,
     images: {
@@ -14,3 +16,23 @@ module.exports = removeImports({
         return config;
     }
 })
+
+
+const sentryConfig = withSentryConfig(
+  nextConfig,
+  {
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    silent: true,
+    org: "thibaud-brault",
+    project: "faqmaker",
+  },
+  {
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+  }
+);
+
+module.exports = sentryConfig
---
+++ sentry.client.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://205614d8b705aa1e52a68d3affaef461@o4506702112489472.ingest.sentry.io/4506704576380928",
+  tracesSampleRate: 1,
+  debug: false,
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  integrations: [
+    Sentry.replayIntegration({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+});
