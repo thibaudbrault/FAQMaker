0036194c7f7bbd5bb500c93db169dc7d753a7c94
Thibaud BRAULT <thibaud.brault@gide.net>
2024-02-23 11:29:43 +0100

    :rotating_light: run lint + prettier

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,12 +1,7 @@
-// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
-// The config you add here will be used whenever one of the edge features is loaded.
-// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://205614d8b705aa1e52a68d3affaef461@o4506702112489472.ingest.sentry.io/4506704576380928",
+  dsn: 'https://205614d8b705aa1e52a68d3affaef461@o4506702112489472.ingest.sentry.io/4506704576380928',
   tracesSampleRate: 1,
   debug: false,
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://205614d8b705aa1e52a68d3affaef461@o4506702112489472.ingest.sentry.io/4506704576380928",
+  dsn: 'https://205614d8b705aa1e52a68d3affaef461@o4506702112489472.ingest.sentry.io/4506704576380928',
   tracesSampleRate: 1,
   debug: false,
 });
