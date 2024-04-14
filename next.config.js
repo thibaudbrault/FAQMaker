ee5abc00019f3c1a0db578d1f3587d6d6be7638c
Thibaud Brault <thibaud.brault@protonmail.com>
2024-04-14 19:50:44 +0200

    :wrench: remove usage of sentry

--- next.config.js
+++ next.config.js
@@ -21,21 +21,21 @@ const nextConfig = removeImports({
   },
 });
 
-const sentryConfig = withSentryConfig(
-  nextConfig,
-  {
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    silent: true,
-    org: 'thibaud-brault',
-    project: 'faqmaker',
-  },
-  {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    tunnelRoute: '/monitoring',
-    hideSourceMaps: true,
-    disableLogger: true,
-  },
-);
+// const sentryConfig = withSentryConfig(
+//   nextConfig,
+//   {
+//     authToken: process.env.SENTRY_AUTH_TOKEN,
+//     silent: true,
+//     org: 'thibaud-brault',
+//     project: 'faqmaker',
+//   },
+//   {
+//     widenClientFileUpload: true,
+//     transpileClientSDK: true,
+//     tunnelRoute: '/monitoring',
+//     hideSourceMaps: true,
+//     disableLogger: true,
+//   },
+// );
 
-module.exports = sentryConfig;
+module.exports = nextConfig;
