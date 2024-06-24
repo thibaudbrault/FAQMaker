957fc24fadb9db5340319fdf18ae2ef64d0eb242
Thibaud BRAULT <thibaud.brault@gide.net>
2024-06-24 16:13:44 +0200

    :fire: remove unused validations files

--- src/lib/validations/favorite.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const favoriteClientSchema = z.object({
-  userId: z.string().cuid2(),
-  nodeId: z.string().cuid2(),
-});
--- src/lib/validations/other.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const csvUploadClientSchema = z.object({
-  name: z.string().min(1, 'Column name is required'),
-});
-
-export const tenantIdSchema = z.object({
-  tenantId: z.string().cuid2(),
-});
