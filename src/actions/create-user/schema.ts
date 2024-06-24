d36dbab2605e4c2a24bad1253dee63737c63bb86
Thibaud BRAULT <thibaud.brault@gide.net>
2024-06-24 16:25:44 +0200

    :label: add min length

--- src/actions/create-user/schema.ts
+++ src/actions/create-user/schema.ts
@@ -3,7 +3,11 @@ import { z } from 'zod';
 import { ROLE } from '@/utils';
 
 export const createUserSchema = z.object({
-  email: z.string().trim().email({ message: 'Invalid email' }),
+  email: z
+    .string()
+    .trim()
+    .min(1, { message: 'User email is required' })
+    .email({ message: 'Invalid email' }),
   role: z.enum(ROLE),
   tenantId: z.string().cuid2(),
   usersCount: z.number().min(0),
