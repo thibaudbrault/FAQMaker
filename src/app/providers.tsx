3ff3f11b0e8e21c9cfeeb011f56bae492608ad4c
Thibaud BRAULT <thibaud.brault@gide.net>
2024-06-28 11:15:23 +0200

    :fire: remove react-query provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import '@/styles/globals.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Inter, Merriweather } from 'next/font/google';
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'sonner';
@@ -19,58 +18,31 @@ const inter = Inter({
   variable: '--font-inter',
 });
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        gcTime: 60 * 1000,
-        staleTime: 60 * 1000,
-        retry: false,
-        throwOnError: true,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined;
-
-function getQueryClient() {
-  if (typeof window === 'undefined') {
-    return makeQueryClient();
-  }
-  if (!browserQueryClient) browserQueryClient = makeQueryClient();
-  return browserQueryClient;
-}
-
 export default function Providers({ children }) {
-  const queryClient = getQueryClient();
-
   return (
     <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster
-            closeButton
-            richColors
-            position="top-center"
-            toastOptions={{
-              style: { fontSize: '1rem' },
-            }}
-          />
-          <TooltipProvider>
-            <div
-              className={`relative h-full min-h-screen text-gray-12 ${merriweather.variable} ${inter.variable}`}
-            >
-              {children}
-            </div>
-          </TooltipProvider>
-        </ThemeProvider>
-      </QueryClientProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Toaster
+          closeButton
+          richColors
+          position="top-center"
+          toastOptions={{
+            style: { fontSize: '1rem' },
+          }}
+        />
+        <TooltipProvider>
+          <div
+            className={`relative h-full min-h-screen text-gray-12 ${merriweather.variable} ${inter.variable}`}
+          >
+            {children}
+          </div>
+        </TooltipProvider>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
