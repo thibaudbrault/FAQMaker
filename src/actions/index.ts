228c9173499878b9ce21167d3caae072fb7a249e
Thibaud Brault <thibaud.brault@protonmail.com>
2024-06-30 15:14:17 +0200

    :art: change export

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,36 +1,46 @@
-export * from './create-answer';
-export * from './create-favorite';
-export * from './create-node';
-export * from './create-tag';
-export * from './create-tenant';
-export * from './create-user';
-export * from './create-users';
-export * from './delete-favorite';
-export * from './delete-tag';
-export * from './delete-tenant';
-export * from './delete-user';
-export * from './get-favorite';
-export * from './get-favorites';
-export * from './get-integration';
-export * from './get-me';
-export * from './get-node';
-export * from './get-nodes';
-export * from './get-nodes-count';
-export * from './get-search-nodes';
-export * from './get-search-tags';
-export * from './get-signed-logo';
-export * from './get-tags';
-export * from './get-tags-count';
-export * from './get-tenant';
-export * from './get-user';
-export * from './get-user-answers';
-export * from './get-user-questions';
-export * from './get-users';
-export * from './get-users-count';
-export * from './login';
-export * from './update-answer';
-export * from './update-logo';
-export * from './update-node';
-export * from './update-tenant';
-export * from './update-user';
-export * from './upsert-integrations';
+export { createAnswer, createAnswerSchema } from './create-answer';
+export { createFavorite, createFavoriteSchema } from './create-favorite';
+export { createNode, createNodeSchema } from './create-node';
+export { createReaction, createReactionSchema } from './create-reaction';
+export { createTag, createTagSchema } from './create-tag';
+export {
+  createTenant,
+  createTenantCompanySchema,
+  createTenantUserSchema,
+  createTenantSchema,
+} from './create-tenant';
+export { createUser, createUserSchema } from './create-user';
+export { createUsers, createUsersSchema } from './create-users';
+export { deleteFavorite, deleteFavoriteSchema } from './delete-favorite';
+export { deleteTag, deleteTagSchema } from './delete-tag';
+export { deleteTenant, deleteTenantSchema } from './delete-tenant';
+export { deleteUser, deleteUserSchema } from './delete-user';
+export { getFavorite } from './get-favorite';
+export { getFavorites } from './get-favorites';
+export { getIntegration } from './get-integration';
+export { getMe } from './get-me';
+export { getNode } from './get-node';
+export { getPaginatedNodes } from './get-nodes';
+export { getNodesCount } from './get-nodes-count';
+export { getReactions } from './get-reactions';
+export { getSearchNodes } from './get-search-nodes';
+export { getSearchTags } from './get-search-tags';
+export { getSignedLogo } from './get-signed-logo';
+export { getTags } from './get-tags';
+export { getTagsCount } from './get-tags-count';
+export { getTenant } from './get-tenant';
+export { getUser } from './get-user';
+export { getUserAnswers } from './get-user-answers';
+export { getUserQuestions } from './get-user-questions';
+export { getUsers } from './get-users';
+export { getUsersCount } from './get-users-count';
+export { emailSignIn } from './login';
+export { updateAnswer, updateAnswerSchema } from './update-answer';
+export { updateLogo, updateLogoSchema } from './update-logo';
+export { updateNode, updateNodeSchema } from './update-node';
+export { updateTenant, updateTenantSchema } from './update-tenant';
+export { updateUser, updateUserSchema } from './update-user';
+export {
+  upsertIntegrations,
+  upsertIntegrationsSchema,
+} from './upsert-integrations';
