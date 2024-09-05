56c5c8965f204496863dec993b72637675df0e8d
Thibaud BRAULT <thibaud.brault@gide.net>
2024-09-05 11:40:22 +0200

    :recycle: move options in different file

---
+++ src/lib/auth.ts
@@ -0,0 +1,89 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import EmailProvider from 'next-auth/providers/email';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { sendVerificationRequest } from '@/lib';
+import { Routes } from '@/utils';
+import prisma from 'lib/prisma';
+
+import type { NextAuthOptions } from 'next-auth';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      sendVerificationRequest,
+    }),
+  ],
+  pages: {
+    signIn: Routes.SITE.LOGIN,
+    error: Routes.SITE.LOGIN,
+  },
+  callbacks: {
+    async signIn({ profile, account, user }) {
+      if (!account || !user) {
+        return false;
+      }
+      const userExists = await prisma.user.findUnique({
+        where: { email: user?.email },
+      });
+      if (!userExists) {
+        return false;
+      }
+      if (
+        account?.provider === 'google' &&
+        (!userExists.name || !userExists.image)
+      ) {
+        await prisma.user.update({
+          where: { id: userExists?.id },
+          data: {
+            name: profile?.name,
+            image: profile?.picture,
+          },
+        });
+      }
+      return true;
+    },
+    async redirect({ baseUrl }) {
+      return baseUrl;
+    },
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+  session: {
+    strategy: `jwt`,
+    maxAge: 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
