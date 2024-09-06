4c5aa2b0fb2569fbecc7df0522e50def35212f6e
Thibaud Brault <thibaud.brault@protonmail.com>
2024-09-06 16:56:57 +0200

    :fire: remove optional for usersArray

--- src/actions/create-users/schema.ts
+++ src/actions/create-users/schema.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const createUsersSchema = z.object({
   tenantId: z.string().cuid2(),
   usersCount: z.number().min(0),
-  usersArray: z.array(z.string().email()).optional(),
+  usersArray: z.array(z.string().email()),
 });
