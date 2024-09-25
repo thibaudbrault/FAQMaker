944db59871f70558abe24c8829d4b66725758eeb
Thibaud BRAULT <thibaud.brault@gide.net>
2024-09-25 10:36:25 +0200

    :bug: remove error throwing

--- src/actions/get-me/index.ts
+++ src/actions/get-me/index.ts
@@ -11,42 +11,34 @@ import type { Me } from '@/types';
 import type { Session } from 'next-auth';
 
 export const getMe = cache(async (): Promise<Me | null> => {
-  try {
-    const session = await getServerSession(authOptions);
-    const id = session?.user?.id;
-    if (!id) {
-      throw new Error('ID not found');
-    }
-    const me = await prisma.user.findUnique({
-      where: { id },
-      include: { tenant: { select: { company: true, logo: true } } },
-    });
-    if (!me) {
-      return null;
-    }
-    return me as Me;
-  } catch {
-    throw new Error('Error fetching user');
+  const session = await getServerSession(authOptions);
+  const id = session?.user?.id;
+  if (!id) {
+    throw new Error('ID not found');
   }
+  const me = await prisma.user.findUnique({
+    where: { id },
+    include: { tenant: { select: { company: true, logo: true } } },
+  });
+  if (!me) {
+    return null;
+  }
+  return me as Me;
 });
 
 export const getUserId = cache(
   async (session: Session): Promise<string | null> => {
-    try {
-      const id = session?.user?.id;
-      if (!id) {
-        throw new Error('ID not found');
-      }
-      const user = await prisma.user.findUnique({
-        where: { id },
-        select: {
-          id: true,
-        },
-      });
-      const userId = user?.id ?? null;
-      return userId as string;
-    } catch {
-      throw new Error('Error fetching user');
+    const id = session?.user?.id;
+    if (!id) {
+      throw new Error('ID not found');
     }
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+      },
+    });
+    const userId = user?.id ?? null;
+    return userId as string;
   },
 );
