eff7bb5d15fa1d89db803d1e58faca55fa74518c
Thibaud BRAULT <thibaud.brault@gide.net>
2024-09-25 17:02:26 +0200

    :sparkles: create custom alert dialog when user leave page with unsaved form

--- src/hooks/useWarnIfUnsavedChanges.tsx
+++ src/hooks/useWarnIfUnsavedChanges.tsx
@@ -1,57 +1,36 @@
-import { useEffect } from 'react';
+'use client';
+
+import { useState, useEffect } from 'react';
 
 export const useWarnIfUnsavedChanges = (unsaved: boolean) => {
+  const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const [showDialog, setShowDialog] = useState(false);
+
   useEffect(() => {
-    const handleAnchorClick = (e: Event) => {
-      const targetUrl = (e.currentTarget as HTMLAnchorElement).href,
-        currentUrl = window.location.href;
-      if (targetUrl !== currentUrl) {
-        if (window.onbeforeunload) {
-          // @ts-expect-error Expect 1 args
-          const res = window.onbeforeunload();
-          if (!res) {
-            e.preventDefault();
-          }
-        }
+    const handleAnchorClick = (e: MouseEvent) => {
+      const targetUrl = (e.currentTarget as HTMLAnchorElement).href;
+      const currentUrl = window.location.href;
+
+      if (targetUrl !== currentUrl && unsaved) {
+        e.preventDefault();
+        setNextUrl(targetUrl);
+        setShowDialog(true);
       }
     };
 
-    const handleMutation = () => {
-      const anchorElements =
-        document.querySelectorAll<HTMLAnchorElement>('a[href]');
-      anchorElements.forEach((anchor) =>
-        anchor.addEventListener('click', handleAnchorClick),
-      );
-    };
+    // Add event listeners to all anchor elements on mount
+    const anchorElements = document.querySelectorAll('a[href]');
+    anchorElements.forEach((anchor) =>
+      anchor.addEventListener('click', handleAnchorClick),
+    );
 
-    const mutationObserver = new MutationObserver(handleMutation);
-    mutationObserver.observe(document, { childList: true, subtree: true });
-
+    // Cleanup function to remove listeners when unmounting
     return () => {
-      mutationObserver.disconnect();
-      const anchorElements =
-        document.querySelectorAll<HTMLAnchorElement>('a[href]');
       anchorElements.forEach((anchor) =>
         anchor.removeEventListener('click', handleAnchorClick),
       );
     };
-  }, []);
-
-  // @ts-expect-error Wrong type of parameter assigned
-  useEffect(() => {
-    const beforeUnloadHandler = () => {
-      const yes = confirm(
-        'Changes you made has not been saved just yet. Do you wish to proceed anyway?',
-      );
-
-      // you can use this condition for something, example: if you have progressbar setup you might want to cancel the animation
-      // if (!yes) cancelProgressbar();
-
-      return yes;
-    };
-    window.onbeforeunload = unsaved ? beforeUnloadHandler : null;
-
-    return () => (window.onbeforeunload = null);
-     
   }, [unsaved]);
+
+  return { showDialog, setShowDialog, nextUrl, setNextUrl };
 };
