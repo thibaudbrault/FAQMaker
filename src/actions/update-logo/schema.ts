f69691aafcfc8127cf6f69fbb8678d98c869c6c4
Thibaud Brault <thibaud.brault@protonmail.com>
2024-10-06 18:04:02 +0200

    :label: update regex check to use cloudfront url

--- src/actions/update-logo/schema.ts
+++ src/actions/update-logo/schema.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
+const path = process.env.NEXT_PUBLIC_AWS_CLOUDFRONT_URL;
+
 export const updateLogoSchema = z.object({
   url: z
     .string()
     .url()
-    .regex(/^https:\/\/storage\.googleapis\.com\/faqmaker\/logos/),
+    .regex(new RegExp(`^${path}`)),
   id: z.string().cuid2(),
 });
