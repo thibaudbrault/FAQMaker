5396c988996f5f6b949961875188cec5dad99d7b
Thibaud BRAULT <thibaud.brault@gide.net>
2024-10-24 10:56:17 +0200

    :truck: update import path for google svg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { signInAction } from '@/actions';
-import googleIcon from '@/assets/google.svg';
 import { Button } from '@/components';
 import { Routes } from '@/utils';
 
@@ -64,7 +63,7 @@ export default function Page({ searchParams }) {
           weight="bold"
           type="submit"
         >
-          <Image src={googleIcon} alt="" width={28} height={28} />
+          <Image src="/google.svg" alt="" width={28} height={28} />
           Sign In with Google
         </Button>
       </form>
