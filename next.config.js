28e3ca451c9a4e6476d6c6fc64340c72151abfca
Thibaud BRAULT <thibaud.brault@gide.net>
2024-10-24 11:44:49 +0200

    :wrench: update config files

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = removeImports({
   transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
