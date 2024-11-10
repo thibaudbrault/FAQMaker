3f6f6c1f66a945fe31d199162462224a292cad5f
Thibaud Brault <thibaud.brault@protonmail.com>
2024-11-10 17:32:26 +0100

    :wrench: remove sentry

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require('next-remove-imports')();
-const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = removeImports({
   transpilePackages: ['@mdxeditor/editor', 'react-diff-view'],
@@ -24,22 +23,5 @@ const nextConfig = removeImports({
     ignoreBuildErrors: true,
   },
 });
-
-// const sentryConfig = withSentryConfig(
-//   nextConfig,
-//   {
-//     authToken: process.env.SENTRY_AUTH_TOKEN,
-//     silent: true,
-//     org: 'thibaud-brault',
-//     project: 'faqmaker',
-//   },
-//   {
-//     widenClientFileUpload: true,
-//     transpileClientSDK: true,
-//     tunnelRoute: '/monitoring',
-//     hideSourceMaps: true,
-//     disableLogger: true,
-//   },
-// );
 
 module.exports = nextConfig;
